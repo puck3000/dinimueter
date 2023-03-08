e7d0f1104e352a1c63da68c193092bfa3e7ccfb8
Daniel Lerch <daniel.lerch@gmail.com>
2023-03-08 17:44:01 +0100

    fix: adds kasperfeedback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
-import diniMueter from '../../public/DiniMueter_EKdMxHASSIGxHELLASTUDIO_DIN_A1_2023_web.webp'
+import diniMueter from '../../public/POSTER_Interieur.jpg'
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -13,11 +13,18 @@ export default function Home() {
         src={diniMueter}
         alt={'Dini Mueter isch hässig Poster'}
       />
-      <h1>
-        Poster
-        <br />
-        DINI MUETER ISCH HÄSSIG 2023
-      </h1>
+      <div>
+        <h1>
+          Poster
+          <br />
+          DINI MUETER ISCH HÄSSIG
+        </h1>
+        <h2>
+          Feministischer Streik 2023
+          <br />
+          EKdM x hässig x Hella Studio
+        </h2>
+      </div>
 
       <article>
         <p>
@@ -25,13 +32,19 @@ export default function Home() {
           wasserbasierender Tinte (aber dennoch wetterbeständig), ohne
           Lösungsmittel, lichtecht, geruchsneutrale Ausdrucke ohne
           Luftschadstoffe und zu 100% rezyklierbar.
+          <br />
+          Printed in Bern, Schweiz
         </p>
         <p>
           A1 (59,4x84,1cm)
           <br />
           Artwork by Hella Studio
         </p>
-        <p>Please allow 1-2 weeks for shipping.</p>
+        <p>
+          CHF 59.– inkl. Versand (CH)
+          <br />
+          Please allow 1-2 weeks for shipping.
+        </p>
       </article>
       <a
         href='https://buy.stripe.com/9AQ4iE0Ja4zo2XK6op'
@@ -39,10 +52,10 @@ export default function Home() {
         rel='noreferrer'
         className='button'
       >
-        BUY
+        Jetzt kaufen
       </a>
       <aside>
-        <p className='noMB'>no credid card? no problem:</p>
+        <p className='noMB'>Questions:</p>
         <a href='mailto:hi@lookmom.ch'>hi@lookmom.ch</a>
       </aside>
     </main>
